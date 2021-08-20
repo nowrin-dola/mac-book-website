309fe24c4e5e089b6e47dd260cac841e1ce7aa41
nowrin-dola <nowrinsiddiquedola@gmail.com>
2021-08-21 04:21:18 +0600

    added content

---
+++ js/app.js
@@ -0,0 +1,62 @@
+document.getElementById('memory-8-btn').addEventListener('click', function () {
+    const depositTotal = document.getElementById('memory-price');
+    depositTotal.innerText = 0;
+
+
+
+
+});
+
+document.getElementById('memory-16-btn').addEventListener('click', function () {
+
+
+    const depositTotal = document.getElementById('memory-price');
+    depositTotal.innerText = 180;
+
+
+});
+document.getElementById('storage-btn-256GB').addEventListener('click', function () {
+
+
+    const depositTotal = document.getElementById('storage-price');
+    depositTotal.innerText = 0;
+
+
+});
+document.getElementById('storage-btn-512GB').addEventListener('click', function () {
+
+
+    const depositTotal = document.getElementById('storage-price');
+    depositTotal.innerText = 100;
+
+
+});
+document.getElementById('storage-btn-1TB').addEventListener('click', function () {
+
+
+    const depositTotal = document.getElementById('storage-price');
+    depositTotal.innerText = 180;
+
+
+});
+
+document.getElementById('delivery-btn').addEventListener('click', function () {
+
+
+    const depositTotal = document.getElementById('delivery-cost');
+    depositTotal.innerText = 0;
+
+
+});
+document.getElementById('delivery-btn-with-cost').addEventListener('click', function () {
+
+
+    const depositTotal = document.getElementById('delivery-cost');
+    depositTotal.innerText = 20;
+
+
+});
+
+
+
+
